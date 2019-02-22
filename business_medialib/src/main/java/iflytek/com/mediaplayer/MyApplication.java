1dca165ce9f50c2897bd1cc75ca310bfb305dc41
unknown <haochen12@iflytek.com>
2019-02-22 11:25:05 +0800

    组件化功能模块提交

---
+++ business_medialib/src/main/java/iflytek/com/mediaplayer/MyApplication.java
@@ -0,0 +1,10 @@
+package iflytek.com.mediaplayer;
+
+import android.app.Application;
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
